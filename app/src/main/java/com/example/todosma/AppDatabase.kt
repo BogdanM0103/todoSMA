33937825b3ee300b7c6a126b9663dee0518a9ea1
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-12-20 17:34:19 +0200

    init database and AppDatabase inherits Room database

--- app/src/main/java/com/example/todosma/AppDatabase.kt
+++ app/src/main/java/com/example/todosma/AppDatabase.kt
@@ -1,8 +1,9 @@
 package com.example.todosma
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [User::class], version = 1)
-abstract class AppDataBase {
+abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 }
--- app/src/main/java/com/example/todosma/MainActivity.kt
+++ app/src/main/java/com/example/todosma/MainActivity.kt
@@ -11,11 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.example.todosma.ui.theme.TodoSMATheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        lateinit var dataBase: AppDatabase
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        dataBase = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "my_database",
+        ).build()
+
         enableEdgeToEdge()
         setContent {
             TodoSMATheme {
