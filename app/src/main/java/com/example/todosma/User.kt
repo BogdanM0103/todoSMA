6c3df4532b382f8d9026f3b83cd8b65fa38ea4bb
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-12-20 17:25:12 +0200

    added dao

--- app/src/main/java/com/example/todosma/User.kt
+++ app/src/main/java/com/example/todosma/User.kt
@@ -3,7 +3,7 @@ package com.example.todosma
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "users")
 data class User(
     @PrimaryKey(autoGenerate = true) val id: Int,
     val username: String
