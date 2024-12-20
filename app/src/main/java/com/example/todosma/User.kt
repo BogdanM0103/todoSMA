67608a101f899f57006fcc772b7cafeed4c3763d
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-12-20 17:20:47 +0200

    added user

---
+++ app/src/main/java/com/example/todosma/User.kt
@@ -0,0 +1,10 @@
+package com.example.todosma
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val username: String
+)
